0e158aa8d7dc26c836116785d78b4d327ef27e97
Dil2000 <dnks2000@gmail.com>
2017-08-05 11:01:06 -0400

    server javascript

---
+++ server.js
@@ -0,0 +1,36 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "reserved.html"));
+});
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+app.listen(PORT, function() {
+  console.log("Listening at PORT" + PORT);
+});
+
+
+
